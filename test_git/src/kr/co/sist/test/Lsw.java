d444e6b0b27cd2bc9b72cc8560683bc73db2e11a
user <user@DESKTOP-COJ9858>
2023-08-22 12:45:06 +0900

    머지3

--- test_git/src/kr/co/sist/test/Lsw.java
+++ test_git/src/kr/co/sist/test/Lsw.java
@@ -6,6 +6,7 @@ public class Lsw {
 		System.out.println("이거슨 신기능 추가 ! ! !");
 		System.out.println("대포동 미사일");
 		System.out.println("임태균의 미소 !! ~_~");
+		System.out.println("임태균의 미소2 !! ^_^");
 	}
 
 }
